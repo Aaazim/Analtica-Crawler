d867a347909c61e98fa174c2a9a83c7b5ac607a3
aazim <warcode91@gmail.com>
2018-06-03 06:21:01 +0530

    Adding files first time

---
+++ myTool/models.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+class Queries(models.Model):
+    user_id=models.CharField(max_length=200)
+    query=models.CharField(max_length=200)
---
+++ myTool/views.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+
+# Create your views here.
+from django.http import HttpResponse
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404, render
+from bs4 import BeautifulSoup
+import requests
+import json
+import re
+from django.core import serializers
+# from basicapp.models import QueryForm
+
+
+def index(request):
+        posts = 'aazim'  # Getting all the posts from database
+        return render(request, 'myTool/post_list.html', { 'posts': posts })
+
+def likePost(request):
+    if request.method == 'GET':
+		print request.GET.has_key('post_id')
+		if request.GET.has_key('post_id'):
+			post_id = request.GET['post_id']
+			# print '------------------------------',post_id
+			likedpost = get_html_tags(str(post_id)) #getting the liked posts
+			return HttpResponse(json.dumps({'url':post_id,'tags':likedpost}), content_type="application/json")
+		elif request.GET.has_key('url_input'):
+			post_id_1 = request.GET['url_input']
+			tag_id = request.GET['tag_id']
+			main_data = get_selected_tag_data(post_id_1,tag_id)
+			# print type(list(main_data))
+			return HttpResponse(json.dumps({'html_data':main_data}), content_type="application/json")
+    else:
+       return HttpResponse("Request method is not a GET")
+
+def get_html_tags(url):
+	page = requests.get(url)
+	soup = BeautifulSoup(page.content,'html.parser')
+	tags = list(set([tag.name for tag in soup.find_all()]))
+	print '-----------------------------------------------------'
+	# print list(soup(text=re.compile('Technicolor')))
+	return tags
+
+def get_selected_tag_data(url,tag):
+	page = requests.get(url)
+	soup = BeautifulSoup(page.content,'html.parser')
+	# tags = list(set([tag.name for tag in soup.find_all()]))
+	# return soup.find_all(tag)
+	return list(soup(text=re.compile(tag)))
+	# print soup(text=re.compile('exact text'))
+
